4253e8dd29a77307240c3e47058c55406067081d
Aryan Varshney <aryanv@gmail.com>
2019-06-20 22:37:42 -0700

    Updates

---
+++ precache-manifest.7920e82abede6e7590e355329173691d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6ac667f86a1db39125ab711542b22f",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "86f9eac607523768d9f1",
+    "url": "/Pokedex/static/css/main.ca138304.chunk.css"
+  },
+  {
+    "revision": "29bcdd7d13475dbb346b",
+    "url": "/Pokedex/static/js/2.f5a82c0a.chunk.js"
+  },
+  {
+    "revision": "86f9eac607523768d9f1",
+    "url": "/Pokedex/static/js/main.1f56b2f3.chunk.js"
+  },
+  {
+    "revision": "119fa3083960fadc0325",
+    "url": "/Pokedex/static/js/runtime~main.cf3900f6.js"
+  }
+]);
